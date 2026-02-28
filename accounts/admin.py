8385f32e58d59b5be5cbc8425ea41cf97d207edf
Opio Daniel <danielopio540@gmail.com>
2026-02-28 10:51:55 +0300

    register of models in the admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Add 'role' to the list view (the main table)
+    list_display = ('username', 'email', 'role', 'is_staff')
+
+    # Add 'role' to the filter sidebar
+    list_filter = ('role', 'is_staff', 'is_superuser')
+
+    # This adds the 'role' field to the User Edit/Add pages
+    # We append it to the existing 'Personal info' section or create a new one
+    fieldsets = UserAdmin.fieldsets + (
+        ('Profile Role', {'fields': ('role',)}),
+    )
+
+    # This ensures the 'role' field is also there when creating a user
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Profile Role', {'fields': ('role',)}),
+    )
--- blood_map/admin.py
+++ blood_map/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import DonorProfile, HospitalProfile
 
-# Register your models here.
+
+# We use GISModelAdmin to activate the Map Widget
+@admin.register(DonorProfile)
+class DonorAdmin(admin.GISModelAdmin):
+    list_display = ('user', 'blood_group', 'is_available', 'location')
+    list_filter = ('blood_group', 'is_available')
+
+    # Coordinates for Uganda (Centering the map)
+    default_lat = 1.3733
+    default_lon = 32.2903
+    default_zoom = 7
+
+    # Uses OpenStreetMap instead of the default vector map
+    gis_widget_kwargs = {
+        'attrs': {
+            'default_lat': 1.3733,
+            'default_lon': 32.2903,
+        }
+    }
+
+
+@admin.register(HospitalProfile)
+class HospitalAdmin(admin.GISModelAdmin):
+    list_display = ('name', 'location')
+    default_lat = 1.3733
+    default_lon = 32.2903
+    default_zoom = 7
